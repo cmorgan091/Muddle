1ceaa05f47b7108bdb8a5f59bb7818c69848f930
Chris Morgan <chris@morganits.com>
2020-09-03 20:19:50 +0100

    Start adding mapview

---
+++ Muddle.Web/Pages/MapView.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Muddle.Domain.Models;
+using Muddle.Sample.Maps;
+
+namespace Muddle.Web.Pages
+{
+    public class MapViewModel : PageModel
+    {
+        private readonly ILogger<MapViewModel> _logger;
+
+        public MapViewModel(ILogger<MapViewModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
